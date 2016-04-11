d8f0e68416de4c6e697d0d04cb497d10f3fec04e
4p3x <1337mm@gmail.com>
2016-04-11 22:01:23 +0200

    listdir

--- sftp.py
+++ sftp.py
@@ -7,9 +7,12 @@ print("### sftp transfer ###")
 
 def transfer(server,user,rsakey,rsakeypass):
     sftp = pysftp.Connection(server,username=user,private_key=rsakey,private_key_pass=rsakeypass)
-    sftp.listdir(remotepath='.')
+    remote_dir = sftp.listdir()
     #sftp.put("localfile", "remotefile")
     sftp.close()
+
+    for i in remote_dir:
+        print i
 
 ##server = raw_input("enter server: ")
 ##user = raw_input("enter username: ")
